a036119dc29bbe2325cef7d8530efc4878f80510
Ynsaibhargavi <117625655+Ynsaibhargavi@users.noreply.github.com>
2022-11-09 16:58:11 +0530

    Update and rename MH-CET.h to MHCET.h

--- CUT/Code/include/MHCET.h
+++ CUT/Code/include/MHCET.h
@@ -1,23 +1,23 @@
-#ifndef MH-CET_H
-#define MH-CET_H
+#ifndef MHCET_H
+#define MHCET_H
 
 using namespace std;
-class MH-CET{
+class MHCET{
 	public:
-		MH-CET();
-		MH-CET(string sub1,string sub2,string sub3){
+		MHCET();
+		MHCET(string sub1,string sub2,string sub3){
 			this->sub1=sub1;
 			this->sub2=sub2;
 			this->sub3=sub3;
 		}
-		~MH-CET();
+		~MHCET();
 		void setsub1(string sub1);
 		string getsub1();
 		void setsub2(string sub2);
 		string getsub2();
 		void setsub3(string sub3);
 		string getsub3();
-		virtual void displayMH-CET();
+		virtual void displayMHCET();
 	private:
 		string sub1,string sub2,string sub3;
 };
